b9514e7acf7932d3ea1e816645be2863869d6050
Rahul Garg <rahul.garg@amd.com>
2025-06-20 02:27:10 -0400

    vllm 0610 update (#58)
    * vllm 0610 update
    
    * Remove duplicate line
    
    * Update docker
    
    * Update docker

--- scripts/vllm/run.sh
+++ scripts/vllm/run.sh
@@ -34,14 +34,16 @@ fi
 export HF_TOKEN=$MAD_SECRETS_HFTOKEN
 
 # Parse named arguments
+BENCHMARK_ARGS=""
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --model_repo) MODEL_NAME="$2"; shift ;;
         --test_option) TEST_OPTION="$2"; shift ;;
         --num_gpu) N_GPUS="$2"; shift ;;
         --datatype) DTYPE="$2"; shift ;;
-        --tunableop) TUNABLEOP="$2"; shift ;;
-        *) echo "Unknown parameter passed: $1"; usage ;;
+        --vllm_version) VLLM_VERSION="$2"; shift ;;
+        --aiter) AITER="$2"; shift ;;
+        *) BENCHMARK_ARGS="$BENCHMARK_ARGS $1" ;;
     esac
     shift
 done
@@ -58,25 +60,30 @@ do
   DTYPE_SP="$DTYPE_SP $i"
 done
 
-if [[ "$TUNABLEOP" == "on" ]]; then 
-    echo "turning on pytorch tunableop"
-    export PYTORCH_TUNABLEOP_ENABLED=1
-else
-    echo "turning off pytorch tunableop"
-    export PYTORCH_TUNABLEOP_ENABLED=0
-fi
-
 export HF_HUB_CACHE="/myworkspace"
 
 echo "=hyper params start="
 echo $MODEL_NAME
 echo $TEST_OPTION_SP
 echo $DTYPE_SP
-echo $PYTORCH_TUNABLEOP_ENABLED
+echo $VLLM_VERSION
+echo $AITER
 echo "=hyper params end="
+
+if [ $VLLM_VERSION == "0" ]; then
+    export VLLM_USE_V1=0
+else
+    export VLLM_USE_V1=1
+fi
+
+if [ $AITER == "on" ]; then
+    export VLLM_ROCM_USE_AITER=1
+else
+    export VLLM_ROCM_USE_AITER=0
+fi
 
 for scenario in $TEST_OPTION_SP; do
     for dtype in $DTYPE_SP; do
-        ./vllm_benchmark_report.sh -s $scenario -m $MODEL_NAME -g $N_GPUS -d $dtype
+        ./vllm_benchmark_report.sh -s $scenario -m $MODEL_NAME -g $N_GPUS -d $dtype $BENCHMARK_ARGS
     done
 done
