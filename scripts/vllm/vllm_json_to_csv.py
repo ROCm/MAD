28e24583e321961e0472bbafe841bf1b6b62b1c1
Rahul Garg <rahul.garg@amd.com>
2026-01-14 19:30:02 -0500

    vLLM Refactor (#124)
    vLLM refactor: add vllm_cmd and update csv parser to add additional columns

--- scripts/vllm/vllm_json_to_csv.py
+++ scripts/vllm/vllm_json_to_csv.py
@@ -2,77 +2,124 @@ import csv, sys, json
 import argparse
 import os
 
+# csv header
+header = [
+    "model",
+    "benchmark",
+    "tp",
+    "inp",
+    "out",
+    "dtype",
+    "num_prompts",
+    "max_concurrency",
+    "bs",
+    "cmd",
+    "performance",
+    "metric",
+    "unit",
+]
+
 # parse arg
-parser = argparse.ArgumentParser(description='Convert vllm json output format to perf csv output format')
-parser.add_argument("--benchmark",
-                        help="latency or throughput or serving")
-parser.add_argument("--model",
-                        type=str,
-                        help="model name")
-parser.add_argument("--vllm_json",
-                        help="path to the vllm json file")
-parser.add_argument("--output_csv",
-                        help="path to the perf csv file")
-parser.add_argument("--tensor-parallel-size",
-                        type=str,
-                        help="tensor parallel size")
-parser.add_argument("--batch-size",
-                        type=str,
-                        help="batch size")
-parser.add_argument("--num-prompts",
-                        type=str,
-                        help="number of prompts")
-parser.add_argument("--max-concurrency",
-                        type=str,
-                        help="max concurrency (serving only)")
-parser.add_argument("--input-len",
-                        type=str,
-                        help="input seq length")
-parser.add_argument("--output-len",
-                        type=str,
-                        help="output gen length")
-parser.add_argument("--dtype",
-                        type=str,
-                        help="data type")
+parser = argparse.ArgumentParser(
+    description="Convert vllm json output format to perf csv output format"
+)
+parser.add_argument("--benchmark", help="latency or throughput or serving")
+parser.add_argument("--model", type=str, help="model name")
+parser.add_argument("--vllm_json", help="path to the vllm json file")
+parser.add_argument("--output_csv", help="path to the perf csv file")
+parser.add_argument("--tensor-parallel-size", type=str, help="tensor parallel size")
+parser.add_argument("--batch-size", type=str, help="batch size")
+parser.add_argument("--num-prompts", type=str, help="number of prompts")
+parser.add_argument(
+    "--max-concurrency", type=str, help="max concurrency (serving only)"
+)
+parser.add_argument("--input-len", type=str, help="input seq length")
+parser.add_argument("--output-len", type=str, help="output gen length")
+parser.add_argument("--dtype", type=str, help="data type")
+parser.add_argument("--vllm-cmd", help="vllm run command")
 
 # read args
 args = parser.parse_args()
 
-with open(args.vllm_json, newline='') as inpf:
-    header_write = 0 if os.path.exists(args.output_csv) else 1
-    with open(args.output_csv,'a+',newline='') as outf:
-        writer = csv.writer(outf, delimiter=',')
+# write json to csv
+header_write = 0 if os.path.exists(args.output_csv) else 1
+with open(args.output_csv, "a+", newline="") as outf:
+    writer = csv.DictWriter(outf, delimiter=",", fieldnames=header)
+    if header_write:
+        writer.writeheader()
+    with open(args.vllm_json, newline="") as inpf:
         reader = json.load(inpf)
         try:
             if args.benchmark == "latency":
-                if(reader["avg_latency"] != 0):
-                    writer.writerow(['model', 'tp', 'bs', 'in', 'out', 'dtype', 'avg_latency (sec)']) if header_write else None
-                    writer.writerow([args.model, args.tensor_parallel_size, args.batch_size, args.input_len, args.output_len, args.dtype, str(reader["avg_latency"])])
+                if reader["avg_latency"] != 0:
+                    row = {
+                        "model": args.model,
+                        "benchmark": args.benchmark,
+                        "tp": args.tensor_parallel_size,
+                        "inp": args.input_len,
+                        "out": args.output_len,
+                        "dtype": args.dtype,
+                        "bs": args.batch_size,
+                        "cmd": args.vllm_cmd,
+                        "performance": str(reader["avg_latency"]),
+                        "metric": "avg_latency",
+                        "unit": "sec",
+                    }
+                    writer.writerow(row)
             elif args.benchmark == "throughput":
-                if(reader["tokens_per_second"] != 0):
-                    writer.writerow(['model', 'tp', 'num_prompts', 'in', 'out', 'dtype', 'throughput_tot (tok/sec)', 'throughput_gen (tok/sec)']) if header_write else None
+                if reader["tokens_per_second"] != 0:
                     elapsed_time = reader["elapsed_time"]
-                    throughput_gen = str(int(int(args.num_prompts) * int(args.output_len) / elapsed_time))
-                    writer.writerow([args.model, args.tensor_parallel_size, args.num_prompts, args.input_len, args.output_len, args.dtype, str(reader["tokens_per_second"]), throughput_gen])
-            elif args.benchmark == "serving":
-                if(reader["total_token_throughput"] != 0):
-                    writer.writerow(['model', 'tp', 'max_concurrency', 'num_prompts', 'in', 'out', 'dtype', 'throughput_tot (tok/sec)', 'throughput_gen (tok/sec)', 'median_tpot (ms)', 'median_itl (ms)', 'median_e2el (ms)']) if header_write else None
-                    writer.writerow(
-                        [
-                            args.model,
-                            args.tensor_parallel_size,
-                            args.max_concurrency,
-                            args.num_prompts,
-                            args.input_len,
-                            args.output_len,
-                            args.dtype,
-                            str(reader["total_token_throughput"]),
-                            str(reader["output_throughput"]),
-                            str(reader["median_tpot_ms"]),
-                            str(reader["median_itl_ms"]),
-                            str(reader["median_e2el_ms"])
-                        ]
+                    throughput_gen = str(
+                        int(int(args.num_prompts) * int(args.output_len) / elapsed_time)
                     )
+                    metrics = {
+                        "throughput_tot": str(reader["tokens_per_second"]),
+                        "throughput_gen": throughput_gen,
+                    }
+                    for metric, perf in metrics.items():
+                        row = {
+                            "model": args.model,
+                            "benchmark": args.benchmark,
+                            "tp": args.tensor_parallel_size,
+                            "inp": args.input_len,
+                            "out": args.output_len,
+                            "dtype": args.dtype,
+                            "num_prompts": args.num_prompts,
+                            "cmd": args.vllm_cmd,
+                            "performance": perf,
+                            "metric": metric,
+                            "unit": "tok/sec",
+                        }
+                        writer.writerow(row)
+            elif args.benchmark == "serving":
+                if reader["total_token_throughput"] != 0:
+                    metrics = {
+                        "throughput_tot": str(reader["total_token_throughput"]),
+                        "throughput_gen": str(reader["output_throughput"]),
+                        "median_tpot": str(reader["median_tpot_ms"]),
+                        "median_itl": str(reader["median_itl_ms"]),
+                        "median_e2el": str(reader["median_e2el_ms"]),
+                    }
+                    for metric, perf in metrics.items():
+                        if "throughput" in metric:
+                            unit = "tok/sec"
+                        else:
+                            unit = "ms"
+                        row = {
+                            "model": args.model,
+                            "benchmark": args.benchmark,
+                            "tp": args.tensor_parallel_size,
+                            "inp": args.input_len,
+                            "out": args.output_len,
+                            "dtype": args.dtype,
+                            "num_prompts": args.num_prompts,
+                            "max_concurrency": args.max_concurrency,
+                            "cmd": args.vllm_cmd,
+                            "performance": perf,
+                            "metric": metric,
+                            "unit": unit,
+                        }
+                        writer.writerow(row)
 
         except csv.Error as e:
-            sys.exit('file {}, line {}: {}'.format(args.vllm_json, reader.line_num, e))
+            sys.exit("file {}, line {}: {}".format(args.vllm_json, reader.line_num, e))
